7e4dfac991d02e0500afc1c527c8e7eb02310975
kiko-bt <58534315+kiko-bt@users.noreply.github.com>
2021-02-07 18:47:37 +0100

    Add files via upload

---
+++ Stopwatch Exercise/Stopwatch Exercise/Stopwatch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stopwatch
+{
+    public class Stopwatch
+    {
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+
+        public void Start()
+        {
+            StartTime = DateTime.Now;
+        }
+
+
+        public void Stop()
+        {
+            EndTime = DateTime.Now;
+            TimeSpan duration = StartTime - EndTime;
+            Console.WriteLine($"Duration of {duration.TotalSeconds} sek");
+
+        }
+    }
+}
